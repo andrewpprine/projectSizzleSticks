47cc88e611d20ad5e097ea446c5e37e09d6bff02
Andrew Prine <andrewpprine@gmail.com>
2018-07-12 06:57:18 -0700

    results to html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -30,6 +30,6 @@ $('button').click(function (){
     console.log(response.response.totalResults);      
     console.log(response.response.groups[0].items[0].venue.name);   
     console.log(response.response.groups[0].items[0].venue.location.formattedAddress);   
-
+    $('#foursquarevenue').html(response.response.groups[0].items[0].venue.name+`<br>`+response.response.groups[0].items[0].venue.location.formattedAddress[0]+`<br>`+response.response.groups[0].items[0].venue.location.formattedAddress[1])
   });
 });
