431e33af252ff92746501794cefeb6538d29f295
Andrew Prine <andrewpprine@gmail.com>
2018-07-18 11:23:40 -0700

    hiding results until click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,34 @@
-var city ;
+var city;
+
 // this is the master function that should call from all API's when run
 $('button').on('click', function(){
- event.preventDefault();
- city = $('#travelWhere').val();
+  $('#showAfterClick').show();
+  event.preventDefault();
+  city = $('#travelWhere').val();
 
- // this is the section for adding ticketmaster events to the web page
- var ticketMasterURL = 'https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=afo4Ma9VAh5dmYQLIfzmuB2zOS0PQXVK&city=' + city;
- $.ajax({
-   url: ticketMasterURL
-     }).then(function(res) {
-          for (var i=0; i< 5; i++){
-     //note this is a placeholder selector until HTML is final
-    var selector = '#ticketmaster' + i.toString();
-    $(selector).append(res._embedded.events[i].name)
-          }
-     });
+  // this is the section for adding ticketmaster events to the web page
+  var ticketMasterURL = 'https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=afo4Ma9VAh5dmYQLIfzmuB2zOS0PQXVK&city=' + city;
+  $.ajax({
+    url: ticketMasterURL
+      }).then(function(res) {
+            for (var i=0; i<10; i++){
+      //note this is a placeholder selector until HTML is final
+      var selector = '#ticketmaster' + i.toString();
+      $(selector).append(res._embedded.events[i].name)
+            }
+      });
 
- // this is the section for adding a google map to the web page
- var googleMapsURL = 'https://www.google.com/maps/embed/v1/search?key=AIzaSyAXGParj76SrKimNk9-iiALLFLiQ0StCB4&q=dallas' + city;
-   $('iframe').attr('src', googleMapsURL);
+  // this is the section for adding a google map to the web page
+  var googleMapsURL = 'https://www.google.com/maps/embed/v1/search?key=AIzaSyAXGParj76SrKimNk9-iiALLFLiQ0StCB4&q=dallas' + city;
+    $('iframe').attr('src', googleMapsURL);
 
- // this is the section for adding weather to the web page
- // this is the section for adding something else to thoe web page
- // this is the section for adding sothing else
+  // this is the section for adding weather to the web page
+  // this is the section for adding something else to thoe web page
+  // this is the section for adding sothing else
 
- var APIKey = "8b2d45874149dd9daa82ef8b500f490d";
- var openweathercurrentURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
- var openweatherURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIKey;
+  var APIKey = "8b2d45874149dd9daa82ef8b500f490d";
+  var openweathercurrentURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
+  var openweatherURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIKey;
 
  //current weather from openweather API
  $.ajax({
@@ -36,7 +38,7 @@ $('button').on('click', function(){
 
   var plusZero = $("<div>").text(response.weather[0].description);
 
-  $("#weatherZero").text(response.main.temp).append(plusZero);  
+  $("#weather1").text(response.main.temp).append(plusZero);  
 
 });
 //forecast weather from openweather API
@@ -50,11 +52,11 @@ $.ajax({
     plusTwo= $("<div>").text(responseList[10].weather[0].description),
     plusThree= $("<div>").text(responseList[18].weather[0].description);
 
-    $("#weatherOne").text(responseList[2].main.temp).append(plusOne);
+    $("#weather2").text(responseList[2].main.temp).append(plusOne);
       
-    $("#weatherTwo").text(responseList[10].main.temp).append(plusTwo);
+    $("#weather3").text(responseList[10].main.temp).append(plusTwo);
       
-    $("#weatherThree").text(responseList[18].main.temp).append(plusThree);
+    $("#weather4").text(responseList[18].main.temp).append(plusThree);
     
   });
   
@@ -84,11 +86,10 @@ $.ajax({
       console.log(body);
     }
   }).then(function(response) {
-    for(x=0;x<10;x++){
-      var selector = '#foursquare' + x.toString();
+    console.log(response);
 
-      console.log(response);
-      
+    for(x=0;x<10;x++){
+      var selector = '#foursquare' + x.toString();      
       var newList = $("<li>").text(response.response.groups[0].items[x].venue.name);
       $(selector).html(newList);
       
